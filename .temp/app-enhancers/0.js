3b6901003378e737179c517563acb567c53e61af
254620494 <39905034+254620494@users.noreply.github.com>
2021-01-26 21:41:43 +0800

    deploy

--- .temp/app-enhancers/0.js
+++ .temp/app-enhancers/0.js
@@ -1,2 +1,2 @@
-import "D:\\Damon\\press\\node_modules\\@vuepress\\core\\lib\\node\\internal-plugins\\style\\client.js"
+import "d:\\2021Persist\\record\\code\\press\\node_modules\\_@vuepress_core@1.8.0@@vuepress\\core\\lib\\node\\internal-plugins\\style\\client.js"
 export default {}
--- text.sh
+++ text.sh
@@ -1,19 +1,19 @@
 #!/bin/sh 
 
 # 确保脚本抛出遇到的错误
-#set -e
+set -e
 
 # 生成静态文件
-#npm run build
+# npm run build
 
 
 cd docs
 cd .vuepress
-cp -r dist/*  -t  ../../../../html/press/
+cp -r dist/*  -t  ../../../../html/
 
 git add -A
 git commit -m 'deploy'
 
 
 # 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
-# git push -f git@github.com:254620494@qq.com/press.git master:gh-pages
+git push -f git@github.com:254620494@qq.com/press.git master:gh-pages
